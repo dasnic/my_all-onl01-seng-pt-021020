e9f99e79609fa64358b85b359508ec0c6ed36364
Lydell Gaines <dasnic70@gmail.com>
2020-02-20 20:13:54 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
-def my_all?(collection)
+def my_all?(1, 2, 3)
 #binding.pry
  i = 0
-  while i < collection.length
-  yield(collection[i])
-    i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
+  while i < 3
+  yield(1)
+    i = 0 + 1   #i += 1 does the same thing. Use this if it's easier for you.
   end
-end
+en
