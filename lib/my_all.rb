1f159a7f1a6ecd4c9585ed51fdc0f2314f8af8c5
Lydell Gaines <dasnic70@gmail.com>
2020-02-20 20:15:06 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
-def my_all?(1, 2, 3)
-#binding.pry
- i = 0
-  while i < 3
-  yield(1)
-    i = 0 + 1   #i += 1 does the same thing. Use this if it's easier for you.
+def my_all?(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
   end
 end
